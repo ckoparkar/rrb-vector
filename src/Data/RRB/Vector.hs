f28a6485b831065f3c6eeea85b0fd5bad53f15f7
Chaitanya Koparkar <ckoparkar@gmail.com>
2019-03-18 13:41:37 -0400

    Add some more ops: snoc, length

--- src/Data/RRB/Vector.hs
+++ src/Data/RRB/Vector.hs
@@ -1,7 +1,12 @@
-module Data.RRB.Vector where
+{-# OPTIONS_GHC -Wno-unused-top-binds #-}
 
-import Data.Bits
-import Debug.Trace
+module Data.RRB.Vector
+  ( Vector , (!), get, snoc, length ) where
+
+import           Data.Bits
+import           Debug.Trace
+import           Prelude hiding (length)
+import qualified Prelude as P
 
 --------------------------------------------------------------------------------
 
@@ -21,14 +26,33 @@ type Height = Int
 type Size   = Int
 
 data Tree a = Leaf [a]
-            | Node Height [Size] [Tree a]
+            | Node { height   :: Height
+                   , sizes    :: [Size]
+                   , subtrees :: [Tree a] }
   deriving (Show, Read, Eq)
 
+type Vector a = Tree a
 
-(!) :: Show a => Tree a -> Int -> a
+----------------------------------------
+-- Length
+----------------------------------------
+
+-- | Return the length of the vector, assuming that 'sizes' is never empty;
+-- which is not how the paper uses it.
+length :: Vector a -> Int
+length tr =
+  case tr of
+    Leaf ns -> P.length ns
+    Node{}  -> last $ sizes tr
+
+----------------------------------------
+-- Lookup
+----------------------------------------
+
+(!) :: Vector a -> Int -> a
 (!) = get
 
-get :: Show a => Tree a -> Int -> a
+get :: Vector a -> Int -> a
 get tr idx =
   case tr of
     Leaf ns -> ns !! idx
@@ -43,7 +67,6 @@ get tr idx =
                          else szs !! (slot' - 1))
       in get (trs !! slot') idx'
 
-
 index_of :: Height -> Int -> Int
 index_of ht i = quot i (m ^ ht) `mod` m
 
@@ -51,50 +74,92 @@ index_of ht i = quot i (m ^ ht) `mod` m
 index_of' :: Height -> Int -> Int
 index_of' ht i = shift i (-1 * ht * log2m) .&. mask
 
+
+----------------------------------------
+-- Insert front/back
+----------------------------------------
+
+snoc :: Vector a -> a -> Vector a
+snoc tr v =
+  case tryBottomRight tr v of
+    Just snocd -> snocd
+    Nothing    -> join tr (mkLeafAtHeight (height tr) v)
+  where
+    tryBottomRight :: Vector a -> a -> Maybe (Vector a)
+    tryBottomRight tr1 v1 =
+      case tr1 of
+        Leaf ns ->
+          if P.length ns < m
+          then Just $ Leaf (ns ++ [v1])
+          else Nothing
+        Node ht szs trs ->
+          let bot_right = last trs
+          in case tryBottomRight bot_right v1 of
+                Just snocd -> let szs' = init szs
+                                  trs' = init trs
+                                  snocd_size = last szs + 1
+                              in Just $ Node ht (szs' ++ [snocd_size]) (trs' ++ [snocd])
+                Nothing    -> if P.length trs < m
+                              then let new_branch = mkLeafAtHeight (ht-1) v1
+                                       -- TODO: Is +1 ok here ?
+                                   in Just $ Node ht (szs ++ [last szs + 1]) (trs ++ [new_branch])
+                              else Nothing
+
+    mkLeafAtHeight :: Height -> a -> Vector a
+    mkLeafAtHeight ht v1 =
+      if ht == 0
+      then Leaf [v1]
+      else Node ht [1] [mkLeafAtHeight (ht-1) v1]
+
+-- | join a b, makes a & b siblings under a new root, assuming that
+-- they're not leaves.
+join :: Vector a -> Vector a -> Vector a
+join a b = Node (height a + 1) [length a, length a + length b] [a,b]
+
 --------------------------------------------------------------------------------
 
 tree10 :: Tree Int
 tree10 = Node 1 [4,8,11]
-                [ Leaf [0,1,2,3]
-                , Leaf [4,5,6,7]
-                , Leaf [8,9,10]
+                [ Leaf [1,2,3,4]
+                , Leaf [5,6,7,8]
+                , Leaf [9,10,11]
                 ]
 
 tree20 :: Tree Int
 tree20 = Node 2 [16,21]
                 [ Node 1 [4,8,12,16]
-                         [ Leaf [0,1,2,3]
-                         , Leaf [4,5,6,7]
-                         , Leaf [8,9,10,11]
-                         , Leaf [12,13,14,15]
+                         [ Leaf [1,2,3,4]
+                         , Leaf [5,6,7,8]
+                         , Leaf [9,10,11,12]
+                         , Leaf [13,14,15,16]
                          ]
                 , Node 1 [4,5]
-                         [ Leaf [16,17,18,19]
-                         , Leaf [20]
+                         [ Leaf [17,18,19,20]
+                         , Leaf [21]
                          ]
                 ]
 
 tree_ub :: Tree Int
 tree_ub = Node 2 [9, 21]
-                 [ Node 1 [2,6,9]
-                             [ Leaf [0,1]
-                             , Leaf [2,3,4,5]
-                             , Leaf [6,7,8]
+                 [ Node 1 [3,6,9]
+                             [ Leaf [1,2,3]
+                             , Leaf [4,5,6]
+                             , Leaf [7,8,9]
                              ]
-                 , Node 1 [2,5,8,12]
-                          [ Leaf [9,10]
-                          , Leaf [11,12,13]
-                          , Leaf [14,15,16]
-                          , Leaf [17,18,19,20]]
+                 , Node 1 [3,6,10,12]
+                          [ Leaf [10,11,12]
+                          , Leaf [13,14,15]
+                          , Leaf [16,17,18,19]
+                          , Leaf [20,21]]
                  ]
 
 
 test1, test2, test3, test4, test5 :: Bool
-test1 = tree_ub ! 20 == 20
-test2 = tree_ub ! 2  == 2
-test3 = tree_ub ! 6  == 6
-test4 = tree10  ! 10 == 10
-test5 = tree20  ! 20 == 20
+test1 = tree_ub ! 20 == 21
+test2 = tree_ub ! 2  == 3
+test3 = tree_ub ! 6  == 7
+test4 = tree10  ! 10 == 11
+test5 = tree20  ! 20 == 21
 
 all_tests :: Bool
 all_tests = all (== True) [test1, test2, test3, test4, test5]
